47149f55c06eda77853c6536ea822a40821b9299
shreshthajit <shreshthajit5@gmail.com>
2024-07-30 14:50:47 +0600

    Basic Layout Design and routing

--- IceCreamMAUI/AppShell.xaml.cs
+++ IceCreamMAUI/AppShell.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace IceCreamMAUI
+﻿using IceCreamMAUI.Pages;
+
+namespace IceCreamMAUI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoute();
+
+        }
+
+        private readonly static Type[] _routablePageTypes =
+            [
+            typeof(SigninPage),
+            typeof(SignupPage),
+            typeof(MyOrdersPage),
+            typeof(OrderDetailsPage),
+            typeof(DetailsPage),
+            ];
+
+        private static void RegisterRoute()
+        {
+            foreach(var pageType in _routablePageTypes)
+            {
+                Routing.RegisterRoute(pageType.Name, pageType);//in the bracket nameof(SigninPage) is route name
+            }
         }
     }
 }
--- IceCreamMAUI/Pages/OnboardingPage.xaml.cs
+++ IceCreamMAUI/Pages/OnboardingPage.xaml.cs
@@ -12,5 +12,13 @@ public partial class OnboardingPage : ContentPage
 		await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
     }
 
-  
+    private async void Signin_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SigninPage));
+    }
+
+    private async void Signup_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(SignupPage));
+    }
 }
